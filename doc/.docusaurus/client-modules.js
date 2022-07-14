399c858fe6b21748b5662bc9b9cc33ae23e980de
siriuslatte <zeydiscorddev.business@gmail.com>
2022-07-14 18:21:02 -0500

    Built the project

---
+++ doc/.docusaurus/client-modules.js
@@ -0,0 +1,6 @@
+export default [
+  require('C:\\Users\\shark\\OneDrive\\Documentos\\Roblox-Rojo\\scaling-plugin\\doc\\node_modules\\infima\\dist\\css\\default\\default.css'),
+  require('C:\\Users\\shark\\OneDrive\\Documentos\\Roblox-Rojo\\scaling-plugin\\doc\\node_modules\\@docusaurus\\theme-classic\\lib\\prism-include-languages'),
+  require('C:\\Users\\shark\\OneDrive\\Documentos\\Roblox-Rojo\\scaling-plugin\\doc\\node_modules\\@docusaurus\\theme-classic\\lib\\nprogress'),
+  require('C:\\Users\\shark\\OneDrive\\Documentos\\Roblox-Rojo\\scaling-plugin\\doc\\src\\css\\custom.css'),
+];
